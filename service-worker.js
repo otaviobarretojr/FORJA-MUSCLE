093de687088eef5645435efd48bbb51f3a1c83e1
otaviobarretojr <85048335+otaviobarretojr@users.noreply.github.com>
2026-08-16 17:26:04 -0400

    v3.4.5: preserva catálogo runtime sem pre-cache total

--- service-worker.js
+++ service-worker.js
@@ -2,6 +2,7 @@ const BUILD='3.4.5';
 const CACHE='forja-muscle-v3-4-5';
 // v3.4.4 compatibility markers: forja-muscle-v3-4-4 • forja-updated • oldForjaCaches.length
 const CORE=['./','./index.html','./refresh.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
+const RUNTIME=['./fragments/home-dashboard.html','./fragments/nutrition.html','./fragments/training.html','./fragments/modals.html','./css/base.css','./css/v13.css','./css/v14.css','./css/v20.css','./css/v21.css','./css/v30.css','./css/v31.css','./css/v32.css','./css/v33.css','./css/v333.css','./css/v334.css','./css/v340.css','./css/v341.css','./css/v342.css','./js/base.js','./js/plan.js','./js/v13.js','./js/enhancements-a.js','./js/v21.js','./js/v30a.js','./js/v30b1.js','./js/v30c.js','./js/dom-fixes.js','./js/v30b2.js','./js/v31.js','./js/v32.js','./js/v33.js','./js/v332.js','./js/v333.js','./js/v334.js','./js/v340.js','./js/v341.js','./js/v342.js'];
 
 self.addEventListener('install',event=>{
   self.skipWaiting();
